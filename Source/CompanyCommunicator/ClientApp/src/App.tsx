c0610a7e798e8491f413aaa77e0db9e91161ead3
Rajasekhara Karri <v-karrr@microsoft.com>
2023-06-05 10:38:21 -0500

    config settings

--- Source/CompanyCommunicator/ClientApp/src/App.tsx
+++ Source/CompanyCommunicator/ClientApp/src/App.tsx
@@ -19,6 +19,7 @@ import { ViewStatusTask } from './components/ViewStatusTask/viewStatusTask';
 import { ROUTE_PARAMS, ROUTE_PARTS } from './routes';
 import { DeleteMessages } from './components/DeleteMessages/deleteMessages';
 import { DeleteConfirmationTask } from './components/DeleteMessages/deleteConfirmationTask';
+import axios from 'axios';
 
 export const App = () => {
   const [fluentUITheme, setFluentUITheme] = React.useState(teamsLightTheme);
@@ -27,6 +28,7 @@ export const App = () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment, @typescript-eslint/prefer-ts-expect-error
   // @ts-ignore
   const dir = i18n.dir(locale);
+  const config: any = axios.defaults;
 
   React.useEffect(() => {
     app
@@ -73,30 +75,33 @@ export const App = () => {
   return (
     <>
       {isAppReady && (
-        <FluentProvider theme={fluentUITheme} dir={dir}>
-          <Suspense fallback={<div></div>}>
-            <BrowserRouter>
-              <Routes>
-                <Route path={`/${ROUTE_PARTS.CONFIG_TAB}`} element={<Configuration />} />
-                <Route path={`/${ROUTE_PARTS.MESSAGES}`} element={<HomePage theme={fluentUITheme} />} />
-                <Route path={`/${ROUTE_PARTS.NEW_MESSAGE}`} element={<NewMessage />} />
-                <Route path={`/${ROUTE_PARTS.DELETE_MESSAGES}`} element={<DeleteMessages theme={fluentUITheme} />} />
-                <Route
-                  path={`/${ROUTE_PARTS.DELETE_MESSAGES_CONFIRM}/:${ROUTE_PARAMS.DELETION_TYPE}/:${ROUTE_PARAMS.DELETION_FROM_DATE}/:${ROUTE_PARAMS.DELETION_TO_DATE}`}
-                  element={<DeleteConfirmationTask />}
-                />
-                <Route path={`/${ROUTE_PARTS.NEW_MESSAGE}/:${ROUTE_PARAMS.ID}`} element={<NewMessage />} />
-                <Route path={`/${ROUTE_PARTS.VIEW_STATUS}/:${ROUTE_PARAMS.ID}`} element={<ViewStatusTask />} />
-                <Route path={`/${ROUTE_PARTS.SEND_CONFIRMATION}/:${ROUTE_PARAMS.ID}`} element={<SendConfirmationTask />} />
-                <Route path={`/${ROUTE_PARTS.SIGN_IN}`} element={<SignInPage />} />
-                <Route path={`/${ROUTE_PARTS.SIGN_IN_SIMPLE_START}`} element={<SignInSimpleStart />} />
-                <Route path={`/${ROUTE_PARTS.SIGN_IN_SIMPLE_END}`} element={<SignInSimpleEnd />} />
-                <Route path={`/${ROUTE_PARTS.ERROR_PAGE}`} element={<ErrorPage />} />
-                <Route path={`/${ROUTE_PARTS.ERROR_PAGE}/:${ROUTE_PARAMS.ID}`} element={<ErrorPage />} />
-              </Routes>
-            </BrowserRouter>
-          </Suspense>
-        </FluentProvider>
+        <>
+          {JSON.stringify(config)}
+          <FluentProvider theme={fluentUITheme} dir={dir}>
+            <Suspense fallback={<div></div>}>
+              <BrowserRouter>
+                <Routes>
+                  <Route path={`/${ROUTE_PARTS.CONFIG_TAB}`} element={<Configuration />} />
+                  <Route path={`/${ROUTE_PARTS.MESSAGES}`} element={<HomePage theme={fluentUITheme} />} />
+                  <Route path={`/${ROUTE_PARTS.NEW_MESSAGE}`} element={<NewMessage />} />
+                  <Route path={`/${ROUTE_PARTS.DELETE_MESSAGES}`} element={<DeleteMessages theme={fluentUITheme} />} />
+                  <Route
+                    path={`/${ROUTE_PARTS.DELETE_MESSAGES_CONFIRM}/:${ROUTE_PARAMS.DELETION_TYPE}/:${ROUTE_PARAMS.DELETION_FROM_DATE}/:${ROUTE_PARAMS.DELETION_TO_DATE}`}
+                    element={<DeleteConfirmationTask />}
+                  />
+                  <Route path={`/${ROUTE_PARTS.NEW_MESSAGE}/:${ROUTE_PARAMS.ID}`} element={<NewMessage />} />
+                  <Route path={`/${ROUTE_PARTS.VIEW_STATUS}/:${ROUTE_PARAMS.ID}`} element={<ViewStatusTask />} />
+                  <Route path={`/${ROUTE_PARTS.SEND_CONFIRMATION}/:${ROUTE_PARAMS.ID}`} element={<SendConfirmationTask />} />
+                  <Route path={`/${ROUTE_PARTS.SIGN_IN}`} element={<SignInPage />} />
+                  <Route path={`/${ROUTE_PARTS.SIGN_IN_SIMPLE_START}`} element={<SignInSimpleStart />} />
+                  <Route path={`/${ROUTE_PARTS.SIGN_IN_SIMPLE_END}`} element={<SignInSimpleEnd />} />
+                  <Route path={`/${ROUTE_PARTS.ERROR_PAGE}`} element={<ErrorPage />} />
+                  <Route path={`/${ROUTE_PARTS.ERROR_PAGE}/:${ROUTE_PARAMS.ID}`} element={<ErrorPage />} />
+                </Routes>
+              </BrowserRouter>
+            </Suspense>
+          </FluentProvider>
+        </>
       )}
     </>
   );
