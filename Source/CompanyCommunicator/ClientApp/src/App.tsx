f260e66278fafcfc1be2f21a06d80ca58fafddb8
Rajasekhara Karri <v-karrr@microsoft.com>
2023-06-06 17:12:02 -0500

    remove redirect

--- Source/CompanyCommunicator/ClientApp/src/App.tsx
+++ Source/CompanyCommunicator/ClientApp/src/App.tsx
@@ -80,7 +80,6 @@ export const App = () => {
       void fetch('https://rajtest2.azurefd.net/api/draftnotifications', {
         method: 'GET',
         headers: { Accept: 'application/json', 'Content-Type': 'application/json', Authorization: 'Bearer ' + token },
-        redirect: 'follow'
       }).then(async r1 => { setResult5(r1?.status); return await r1.json(); }).then(r2 => { setResult(r2); });
       // axios.interceptors.request.use((config) => {
       //   config.headers.Authorization = 'Bearer ' + token;
