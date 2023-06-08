88b43377fd99c93b6c42e272150d205bdb42cef9
Rajasekhara Karri <v-karrr@microsoft.com>
2023-06-08 14:16:30 -0500

    Reset back to json

--- Source/CompanyCommunicator/ClientApp/src/apis/apiDecorator.ts
+++ Source/CompanyCommunicator/ClientApp/src/apis/apiDecorator.ts
@@ -15,9 +15,9 @@ export class ApiDecorator {
   public async get(url: string): Promise<any> {
     return await this.handleAxiosCall('get', url).then((response) => {
       if (response.type === 'cors' && response.status === 401 && isIOSHost()) {
-        return this.handleAxiosCall('get', response.url).then((result) => result.text());
+        return this.handleAxiosCall('get', response.url).then((result) => result.json());
       } else {
-        return response.text();
+        return response.json();
       }
     });
   }
@@ -25,9 +25,9 @@ export class ApiDecorator {
   public async delete(url: string): Promise<any> {
     return await this.handleAxiosCall('delete', url).then((response) => {
       if (response.type === 'cors' && response.status === 401 && isIOSHost()) {
-        return this.handleAxiosCall('delete', response.url).then((result) => result.text());
+        return this.handleAxiosCall('delete', response.url).then((result) => result.json());
       } else {
-        return response.text();
+        return response.json();
       }
     });
   }
@@ -35,9 +35,9 @@ export class ApiDecorator {
   public async post(url: string, data?: any): Promise<any> {
     return await this.handleAxiosCall('post', url, data).then((response) => {
       if (response.type === 'cors' && response.status === 401 && isIOSHost()) {
-        return this.handleAxiosCall('post', response.url, data).then((result) => result.text());
+        return this.handleAxiosCall('post', response.url, data).then((result) => result.json());
       } else {
-        return response.text();
+        return response.json();
       }
     });
   }
@@ -45,9 +45,9 @@ export class ApiDecorator {
   public async put(url: string, data?: any): Promise<any> {
     return await this.handleAxiosCall('put', url, data).then((response) => {
       if (response.type === 'cors' && response.status === 401 && isIOSHost()) {
-        return this.handleAxiosCall('put', response.url, data).then((result) => result.text());
+        return this.handleAxiosCall('put', response.url, data).then((result) => result.json());
       } else {
-        return response.text();
+        return response.json();
       }
     });
   }
